8cde460dc5b33a8552812c4b9925ec06e4950a0b
vladi <vladikadiroff@gmail.com>
2021-04-06 18:57:28 +0300

    compound LoadingFooterViewHolder.kt with LoadStateAdapter.kt

--- app/src/main/java/ru/vladikadiroff/pagination/ui/adapters/LoadStateAdapter.kt
+++ app/src/main/java/ru/vladikadiroff/pagination/ui/adapters/LoadStateAdapter.kt
@@ -1,9 +1,13 @@
 package ru.vladikadiroff.pagination.ui.adapters
 
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
-import ru.vladikadiroff.pagination.ui.adapters.viewholders.LoadingFooterViewHolder
+import ru.vladikadiroff.pagination.R
+import ru.vladikadiroff.pagination.databinding.ItemLoadingFooterBinding
+import ru.vladikadiroff.pagination.utils.abstracts.BaseViewHolder
+import ru.vladikadiroff.pagination.ui.adapters.LoadStateAdapter.LoadingFooterViewHolder
 
 class LoadStateAdapter(private val retry: () -> Unit) :
     LoadStateAdapter<LoadingFooterViewHolder>() {
@@ -13,6 +17,20 @@ class LoadStateAdapter(private val retry: () -> Unit) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState) =
-            LoadingFooterViewHolder(parent, retry)
+        LoadingFooterViewHolder(parent, retry)
+
+    class LoadingFooterViewHolder(parent: ViewGroup, retry: () -> Unit) :
+        BaseViewHolder(R.layout.item_loading_footer, parent) {
+
+        private val binding = ItemLoadingFooterBinding.bind(itemView)
+        private val footerLoading = binding.footerLoading
+        private val footerError = binding.footerError.also { it.setOnClickListener { retry() } }
+
+        fun bind(loadState: LoadState) {
+            footerError.isVisible = loadState is LoadState.Error
+            footerLoading.isVisible = loadState is LoadState.Loading
+        }
+
+    }
 
 }
